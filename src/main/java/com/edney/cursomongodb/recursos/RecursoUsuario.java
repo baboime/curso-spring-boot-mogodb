d1aeb165c164d0fb1752040a877c4d2d90b8dcaf
Edney Baboim <ednney@gmail.com>
2020-05-17 14:51:03 -0300

    Implentação do DTO para retornar informação dos usuários

--- src/main/java/com/edney/cursomongodb/recursos/RecursoUsuario.java
+++ src/main/java/com/edney/cursomongodb/recursos/RecursoUsuario.java
@@ -1,6 +1,7 @@
 package com.edney.cursomongodb.recursos;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.edney.cursomongodb.dto.UsuarioDTO;
 import com.edney.cursomongodb.entidades.Usuario;
 import com.edney.cursomongodb.servicos.ServicoUsuario;
 
@@ -19,9 +21,10 @@ public class RecursoUsuario {
 	private ServicoUsuario servico;
 		
 	@RequestMapping(method = RequestMethod.GET) // pode ser utilizado o '@GetMapping'
-	public ResponseEntity<List<Usuario>> buscarTodos() {
+	public ResponseEntity<List<UsuarioDTO>> buscarTodos() {
 		List<Usuario> lista = servico.buscarTodos();
-		return ResponseEntity.ok().body(lista);
+		List<UsuarioDTO> listaDto = lista.stream().map(x -> new UsuarioDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listaDto);
 	}
  
 }

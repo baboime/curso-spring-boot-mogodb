cce282beea588e32468b50f964085fff70bdb1cc
Edney Baboim <ednney@gmail.com>
2020-05-17 13:37:44 -0300

    Criação do projeto

---
+++ src/main/java/com/edney/cursomongodb/CursomongodbApplication.java
@@ -0,0 +1,13 @@
+package com.edney.cursomongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edney/cursomongodb/CursomongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edney.cursomongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
